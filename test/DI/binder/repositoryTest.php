05079ab94c813c3dee60cc65e353dbc39e8b95cb
dev <dev@ubuntu.(none)>
2011-08-18 14:05:14 -0700

    overwrite bindings in a repository

--- test/DI/binder/repositoryTest.php
+++ test/DI/binder/repositoryTest.php
@@ -66,4 +66,18 @@ class binderRepositoryTest extends \PHPUnit_Framework_TestCase {
         $this->assertTrue($repository->getBinding('istd') === $binder1);
         $this->assertTrue($repository->getBinding('istd2') === $binder2);
     }
+
+    function testOverwritebindings() {
+        $repository = new repository();
+
+        $binder1 = new binder('istd');
+        $binder1->to('std');
+        $binder2 = new binder('istd');
+        $binder2->to('std2');
+
+        $repository->addBindings(array($binder1, $binder2));
+
+        $this->assertTrue($repository->getBinding('istd') !== $binder1);
+        $this->assertTrue($repository->getBinding('istd') === $binder2);
+    }
 }
