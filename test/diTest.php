5863803619e0262b93d4f747d29b311cf7b2e3b9
dev <dev@ubuntu.(none)>
2011-09-26 15:22:51 -0700

    add bootstrap file

--- test/diTest.php
+++ test/diTest.php
@@ -4,24 +4,6 @@ namespace de\any\di\test;
 use de\any\di;
 use de\any\di\binder;
 
-require_once __DIR__ . '/../di.php';
-
-array_map(function($v) { include_once  $v; }, glob(__DIR__.'/'.basename(__FILE__,'.php').'/*.php'));
-array_map(function($v) { include_once  $v; }, glob(__DIR__.'/diNestedTest/*.php'));
-array_map(function($v) { include_once  $v; }, glob(__DIR__.'/diConstructor/*.php'));
-array_map(function($v) { include_once  $v; }, glob(__DIR__.'/diDecorateTest/*.php'));
-array_map(function($v) { include_once  $v; }, glob(__DIR__.'/diDecoratorNeedDecorated/*.php'));
-array_map(function($v) { include_once  $v; }, glob(__DIR__.'/diSharedDecorators/*.php'));
-array_map(function($v) { include_once  $v; }, glob(__DIR__.'/diParam/*.php'));
-array_map(function($v) { include_once  $v; }, glob(__DIR__.'/diCircular/*.php'));
-array_map(function($v) { include_once  $v; }, glob(__DIR__.'/diCircularNested/*.php'));
-array_map(function($v) { include_once  $v; }, glob(__DIR__.'/diPropertyParseException/*.php'));
-array_map(function($v) { include_once  $v; }, glob(__DIR__.'/diTestIgnoreAnnotation/*.php'));
-array_map(function($v) { include_once  $v; }, glob(__DIR__.'/diRunable/*.php'));
-array_map(function($v) { include_once  $v; }, glob(__DIR__.'/diRepositoryConcern/*.php'));
-array_map(function($v) { include_once  $v; }, glob(__DIR__.'/diRepositoryInject/*.php'));
-array_map(function($v) { include_once  $v; }, glob(__DIR__.'/diCodingstyle/*.php'));
-
 class DITest extends \PHPUnit_Framework_TestCase {
 
     public function testDiSet() {
